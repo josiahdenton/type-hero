0d5561ebe028f5d62d172517c128188c8c4b6b0f
Josiah Denton <jfranklindenton@gmail.com>
2024-10-25 20:19:50 -0400

    fix: prevent scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import WordBox from './components/WordBox';
 
 function App() {
     return (
-        <div className="flex flex-col p-8 gap-10">
+        <div className="flex flex-col p-8 gap-10 overflow-hidden">
             <h1 className="text-3xl font-bold self-center text-slate-700">
                 Keyboard Type Hero!
             </h1>
