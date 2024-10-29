a3a6dde295abda8867b138ec36ffaec11683f7c1
Josiah Denton <jfranklindenton@gmail.com>
2024-10-28 20:52:00 -0400

    chore: small title change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className={'flex flex-col px-8 pt-8 gap-10 overflow-hidden'}>
             <h1 className="text-3xl font-bold self-center text-slate-500">
-                Keyboard Type Hero!
+                Type Hero!
             </h1>
             <div className="flex flex-col justify-center gap-4 self-center py-16">
                 <Game
